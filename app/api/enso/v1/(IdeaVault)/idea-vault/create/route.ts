a14602917a004dc9b8ee03de77cb41b614519306
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2026-01-01 21:57:36 +0530

    feat: Implement initial IdeaVault API routes for creating, retrieving, and deleting ideas.

--- app/api/enso/v1/(IdeaVault)/idea-vault/create/route.ts
+++ app/api/enso/v1/(IdeaVault)/idea-vault/create/route.ts
@@ -1,5 +1,6 @@
-
+import Idea_Vault_Model from "@/app/api/Models/idea-vault-Modles/idea-vault-Modle";
 import { NextRequest, NextResponse as res } from "next/server";
+import { DB_Connect } from "@/app/api/dbConnect/DB_Connect";
 
 export const GET = () => {
     return res.json({
@@ -8,11 +9,21 @@ export const GET = () => {
     });
 }
 
-export const POST = (req: NextRequest) => {
-    const body = req.json()
+export const POST = async (req: NextRequest) => {
+    await DB_Connect();
 
-    return res.json({
-        ok: true,
-        msg: "Vault will be created here"
-    });
+    const body = await req.json()
+
+    const { title, description } = body
+
+    if (!title || !description) {
+        return res.json({
+            ok: false,
+            errMsg: "title and description are required"
+        }, {
+            status: 400
+        });
+    }
+
+    // first create na user login register api
 }
--- app/api/enso/v1/(IdeaVault)/idea-vault/get/route.ts
+++ app/api/enso/v1/(IdeaVault)/idea-vault/get/route.ts
@@ -1,8 +1,14 @@
-import { NextRequest, NextResponse as res } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = () => {
-    return res.json({
+export async function GET(req: NextRequest) {
+    const param = req.nextUrl.searchParams
+    const id = param.get("id");
+    const idea = param.get("idea");
+
+    return NextResponse.json({
         ok: true,
-        msg: "Vault will be fetched from here"
+        msg: "Vault will be fetched from here",
+        id,
+        idea,
     });
 }
